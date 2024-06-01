39783ac2b7a509c5ce87446b01f9c4a0493749c1
Shankar R <rohithkumar412.t@gmail.com>
2024-06-01 17:50:38 +0530

    new

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -7,15 +7,12 @@ import { faLinkedin, faInstagram, faGithub, faTwitter } from '@fortawesome/free-
 const Footer = () => {
   return (
     <footer>
-      <h4>Thank You For Visiting My Website.</h4>
       <div className="social">
-        <h4>Contact Me - </h4>
         <a href="https://www.linkedin.com/in/rohith-kumar-tetali-04a647296/"><FontAwesomeIcon icon={faLinkedin} /></a>
         <a href="https://instagram.com/_rohith_kumar__?utm_medium=copy_link" target="_blank"><FontAwesomeIcon icon={faInstagram} /></a>
         <a href="https://github.com/RohithKumar08"><FontAwesomeIcon icon={faGithub} /></a>
         <a href="https://twitter.com/_rohith_kumar__"><FontAwesomeIcon icon={faTwitter} /></a>
       </div>
-      <p>© 2024 Rohith Kumar Tetali. All rights reserved.</p>
     </footer>
   );
 }
